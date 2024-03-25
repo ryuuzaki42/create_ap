2dae16230dd3aeb8a1573c6c48df7684a57d43c4
João Batista <joao42lbatista@gmail.com>
2024-03-25 18:31:15 -0300

    Update dl_updates.sh

---
+++ dl_updates.sh
@@ -0,0 +1,5 @@
+
+wget https://raw.githubusercontent.com/lakinduakash/linux-wifi-hotspot/master/src/scripts/create_ap
+wget https://raw.githubusercontent.com/lakinduakash/linux-wifi-hotspot/master/src/scripts/create_ap.conf
+wget https://raw.githubusercontent.com/lakinduakash/linux-wifi-hotspot/master/src/scripts/bash_completion
+wget https://github.com/lakinduakash/linux-wifi-hotspot/blob/master/src/scripts/README.md
