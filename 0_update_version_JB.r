714216698f4eefe1705b68d82228a44ae4931375
João Batista <joao42lbatista@gmail.com>
2025-01-12 15:30:59 -0300

    Update 0_update_version_JB.r - cp instead mv

--- 0_update_version_JB.r
+++ 0_update_version_JB.r
@@ -11,25 +11,28 @@
         cd create_ap-*-noarch-1_JB/
 
     ## See changes
-        smartsynchronize README.md usr/share/doc/create_ap/README.md
-        mv README.md usr/share/doc/create_ap/README.md
+        smartsynchronize ../README.md usr/share/doc/create_ap/README.md
+        cp ../README.md usr/share/doc/create_ap/README.md
 
-        smartsynchronize create_ap usr/bin/create_ap
-        chmod +x create_ap
-        mv create_ap usr/bin/
+        smartsynchronize ../create_ap usr/bin/create_ap
+        chmod +x ../create_ap
+        cp ../create_ap usr/bin/
 
-        smartsynchronize create_ap.conf etc/create_ap.conf
-        mv create_ap.conf etc/create_ap.conf
+        smartsynchronize ../create_ap.conf etc/create_ap.conf
+        cp ../create_ap.conf etc/create_ap.conf
 
-        smartsynchronize bash_completion usr/share/bash-completion/completions/create_ap
-        mv bash_completion usr/share/bash-completion/completions/create_ap
+        smartsynchronize ../bash_completion usr/share/bash-completion/completions/create_ap
+        cp ../bash_completion usr/share/bash-completion/completions/create_ap
 
     ## Make pkg
         cd ../
         su
 
-        version="0.4.8_gitfe64e71"
+        version="0.4.9_git_84af02f"
         mv create_ap-*-noarch-1_JB/ create_ap-$version-noarch-1_JB/
 
-        cd create_ap-*/
+        cd create_ap-$version-noarch-1_JB/
         makepkg -l y -c y ../create_ap-$version-noarch-1_JB.txz
+
+        cd ../
+        rm -r create_ap-$version-noarch-1_JB/
